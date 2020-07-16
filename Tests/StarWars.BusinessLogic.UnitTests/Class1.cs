754ef3520c7019c6e9b018adb4a7c25b9ce3a2fc
Kuba Buda <kubabuda@gazeta.pl>
2020-07-16 23:36:27 +0200

    Added unit tests and integration tests projects

---
+++ Tests/StarWars.BusinessLogic.UnitTests/Class1.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+
+namespace StarWars.BusinessLogic.UnitTests
+{
+    [TestFixture]
+    public class BusinessLogicUnitTests
+    {
+        [Test]
+        public void Test_shouldPass()
+        {
+            (2 + 2).Should().Be(4);
+        }
+
+
+        [Test]
+        public void Test_shouldFail()
+        {
+            (2 + 2).Should().Be(5);
+        }
+    }
+}
