04e5ee6e1c95ced2799a6df6a50cf1b906bbf850
Kuba Buda <kubabuda@gazeta.pl>
2020-07-17 23:59:04 +0200

    Green: implement using repository in service

--- StarWars.BusinessLogic/Services/CharactersService.cs
+++ StarWars.BusinessLogic/Services/CharactersService.cs
@@ -1,4 +1,5 @@
 ﻿using StarWars.BusinessLogic.Interfaces;
+using StarWars.BusinessLogic.Interfaces.Repositories;
 using StarWars.BusinessLogic.Models;
 using StarWars.BusinessLogic.Services.Interfaces;
 using System.Collections.Generic;
@@ -9,64 +10,21 @@ namespace StarWars.BusinessLogic.Services
     public class CharactersService : ICharactersService
     {
 		private readonly IStarWarsApiConfiguration _configuration;
+		private readonly ICharacterRepository _repository;
 
-		public CharactersService(IStarWarsApiConfiguration configuration)
+
+		public CharactersService(IStarWarsApiConfiguration configuration, 
+			ICharacterRepository repository)
         {
 			_configuration = configuration;
+            _repository = repository;
         }
 
-		public IEnumerable<Character> GetCharacters(int pageNr)
+        public IEnumerable<Character> GetCharacters(int pageNr)
         {
-			return _characters.AsQueryable()
+			return _repository.GetQueryable()
 				.Skip((pageNr - 1) * _configuration.PageSize)
 				.Take(_configuration.PageSize);
         }
-
-        private static readonly IEnumerable<Character> _characters = new List<Character>
-		{
-			new Character
-			{
-				Name = "Luke Skywalker",
-				Episodes = new[] { "NEWHOPE", "EMPIRE", "JEDI" },
-				Friends = new[] { "Han Solo", "Leia Organa", "C-3PO", "R2-D2" }
-			},
-			new Character
-			{
-				Name = "Darth Vader",
-				Episodes = new[] { "NEWHOPE", "EMPIRE", "JEDI" },
-				Friends = new[] { "Wilhuff Tarkin" }
-			},
-			new Character
-			{
-				Name = "Han Solo",
-				Episodes = new[] { "NEWHOPE", "EMPIRE", "JEDI" },
-				Friends = new[] { "Luke Skywalker", "Leia Organa", "R2-D2" }
-			},
-			new Character
-			{
-				Name = "Leia Organa",
-				Episodes = new[] { "NEWHOPE", "EMPIRE", "JEDI" },
-				Friends = new[] { "Luke Skywalker", "Han Solo", "C-3PO", "R2-D2" },
-				Planet = "Alderaan"
-			},
-			new Character
-			{
-				Name = "Wilhuff Tarkin",
-				Episodes = new[] { "NEWHOPE" },
-				Friends = new[] { "Darth Vader" }
-			},
-			new Character
-			{
-				Name = "C-3PO",
-				Episodes = new[] { "NEWHOPE", "EMPIRE", "JEDI" },
-				Friends = new[] { "Luke Skywalker", "Han Solo", "Leia Organa", "R2-D2" }
-			},
-			new Character
-			{
-				Name = "R2-D2",
-				Episodes = new[] { "NEWHOPE", "EMPIRE", "JEDI" },
-				Friends = new[] { "Luke Skywalker", "Han Solo", "Leia Organa" }
-			}
-		};
     }
 }
---
+++ StarWars.DataAccess/Repository/CharacterRepository.cs
@@ -0,0 +1,67 @@
+﻿using StarWars.BusinessLogic.Interfaces.Repositories;
+using StarWars.BusinessLogic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StarWars.DataAccess.Repository
+{
+
+    public class CharacterRepository : ICharacterRepository
+    {
+		private static List<Character> s_characters = Initialize();
+
+        public IQueryable<Character> GetQueryable()
+        {
+			return s_characters.AsQueryable();
+        }
+
+		private static List<Character> Initialize() => new List<Character>
+		{
+			new Character
+			{
+				Name = "Luke Skywalker",
+				Episodes = new[] { "NEWHOPE", "EMPIRE", "JEDI" },
+				Friends = new[] { "Han Solo", "Leia Organa", "C-3PO", "R2-D2" }
+			},
+			new Character
+			{
+				Name = "Darth Vader",
+				Episodes = new[] { "NEWHOPE", "EMPIRE", "JEDI" },
+				Friends = new[] { "Wilhuff Tarkin" }
+			},
+			new Character
+			{
+				Name = "Han Solo",
+				Episodes = new[] { "NEWHOPE", "EMPIRE", "JEDI" },
+				Friends = new[] { "Luke Skywalker", "Leia Organa", "R2-D2" }
+			},
+			new Character
+			{
+				Name = "Leia Organa",
+				Episodes = new[] { "NEWHOPE", "EMPIRE", "JEDI" },
+				Friends = new[] { "Luke Skywalker", "Han Solo", "C-3PO", "R2-D2" },
+				Planet = "Alderaan"
+			},
+			new Character
+			{
+				Name = "Wilhuff Tarkin",
+				Episodes = new[] { "NEWHOPE" },
+				Friends = new[] { "Darth Vader" }
+			},
+			new Character
+			{
+				Name = "C-3PO",
+				Episodes = new[] { "NEWHOPE", "EMPIRE", "JEDI" },
+				Friends = new[] { "Luke Skywalker", "Han Solo", "Leia Organa", "R2-D2" }
+			},
+			new Character
+			{
+				Name = "R2-D2",
+				Episodes = new[] { "NEWHOPE", "EMPIRE", "JEDI" },
+				Friends = new[] { "Luke Skywalker", "Han Solo", "Leia Organa" }
+			}
+		};
+	}
+}
