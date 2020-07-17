8d65b63ba2cfd4dea1edf9912f1d3908c94f0599
Kuba Buda <kubabuda@gazeta.pl>
2020-07-17 23:59:30 +0200

    Refactor: register repository in DI container

--- StarWars.Api/Configuration/Bootstrapper.cs
+++ StarWars.Api/Configuration/Bootstrapper.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using StarWars.BusinessLogic.Interfaces;
+using StarWars.BusinessLogic.Interfaces.Repositories;
 using StarWars.BusinessLogic.Services;
 using StarWars.BusinessLogic.Services.Interfaces;
+using StarWars.DataAccess.Repository;
 
 namespace StarWars.Api.Configuration
 {
@@ -13,9 +15,11 @@ namespace StarWars.Api.Configuration
         public static IServiceCollection Bootstrap(this IServiceCollection services, IConfiguration configuration)
         {
             //var appConfiguration = new ApiConfiguration(configuration);
-
             services.AddScoped<IStarWarsApiConfiguration, ApiConfiguration>();
+            // Business logic layer
             services.AddScoped<ICharactersService, CharactersService>();
+            // Data access layer
+            services.AddScoped<ICharacterRepository, CharacterRepository>();
 
             return services;
         }
--- StarWars.DataAccess/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace StarWars.DataAccess
-{
-    public class Class1
-    {
-    }
-}
